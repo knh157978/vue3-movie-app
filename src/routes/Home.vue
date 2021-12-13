6a2e84eef22a623ee61d80d35855a0ecbe4d9a7d
knh157978 <knh1579@gmail.com>
2021-12-13 15:38:03 +0900

    vue3 영화검새

---
+++ src/routes/Home.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container">
+ <Headline/>
+ <Search/>
+ <MovieList/>
+ </div>
+</template>
+
+<script>
+import Headline from '~/components/Headline'
+import Search from '~/components/Search.vue'
+import MovieList from '~/components/MovieList.vue'
+export default {
+   components:{
+       Headline,
+       Search,
+       MovieList
+   },
+   created(){
+       this.$store.commit('movie/resetMovies')
+   }
+}
+</script>
+
